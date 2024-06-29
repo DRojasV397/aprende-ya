083257bb3eaa410a98e0b3c89ee49ba2a5df5d14
DiegoRV397 <diegorv3971@gmail.com>
2024-06-28 20:35:09 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const express = require('express');
 const path = require('path');
 
 // server used to send send emails

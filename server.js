241363d08dfdbfb1d38bffcaddbde9169b70d70c
DiegoRV397 <diegorv3971@gmail.com>
2024-06-28 20:32:23 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const nodemailer = require("nodemailer");
+const express = require('express');
+const path = require('path');
 
 // server used to send send emails
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 const PORT = process.env.PORT || 3000;  // Render establecerá process.env.PORT
+
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

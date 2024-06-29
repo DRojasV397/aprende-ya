320e6f46e0b5ccebbc3bd290690ab4958ed69699
DiegoRV397 <diegorv3971@gmail.com>
2024-06-28 20:00:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+const PORT = process.env.PORT || 3000;  // Render establecerá process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
